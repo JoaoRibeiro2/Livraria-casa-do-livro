e9c0c41449272a7d6531d7a4cde0a2b3fdd54e5c
JoaoRibeiro2 <joaoribeiro2lokao@gmail.com>
2020-07-25 11:50:41 -0300

    Banco corrigido

--- banco/livraria.sql
+++ banco/livraria.sql
@@ -23,28 +23,11 @@ id_usuario int,
 foreign key(id_usuario) references tb_usuario(cd_usuario)
 )default character set = utf8;
 
-create table tb_desejo(
-id_livro int,
-id_usuario int,
-ds_aviso VARCHAR(45),
-foreign key(id_livro) references tb_livro(cd_livro),
-foreign key(id_usuario) references tb_usuario(cd_usuario)
-)default character set = utf8;
-
 create table tb_compra(
 cd_compra int primary key auto_increment,
 dt_compra DATE,
 vl_total_compra REAL,
 id_usuario int
-)default character set = utf8;
-
-create table tb_item_compra(
-id_livro int,
-id_compra int,
-vl_livro real not null,
-qt_livro VARCHAR(45) not null,
-foreign key(id_livro) references tb_livro(cd_livro),
-foreign key(id_compra) references tb_compra(cd_compra)
 )default character set = utf8;
 
 create table tb_categoria(
@@ -64,14 +47,6 @@ cd_autor int primary key auto_increment,
 nm_autor VARCHAR(60) not null,
 nm_foto_autor VARCHAR(45)
 )default character set = utf8;
-
-create table tb_foto(
-cd_foto int primary key auto_increment,
-nm_foto VARCHAR(120) not null,
-id_livro int,
-foreign key (id_livro) references tb_livro(cd_livro)
-)default character set = utf8;
-
 
 create table tb_livro(
 cd_livro int primary key auto_increment,
@@ -99,25 +74,53 @@ foreign key(id_categoria) references tb_categoria(cd_categoria),
 foreign key(id_livro) references tb_livro(cd_livro)
 )default character set = utf8;
 
-/***************** Tabelas Relacionadas ao Usuário ***********************/
-
-create table tb_usuario(
-cd_usuario int primary key auto_increment,
-nm_usuario VARCHAR(60) not null,
-ds_senha VARCHAR(60) not null,
-nm_login VARCHAR(60) not null,
-dt_nascimento DATE not null,
-cd_telefone VARCHAR(20) not null,
-nm_email VARCHAR(60) not null,
-cpf_usuario VARCHAR(45) not null,
-rg_usuario VARCHAR(45) not null
+create table tb_desejo(
+id_livro int,
+id_usuario int,
+ds_aviso VARCHAR(45),
+foreign key(id_livro) references tb_livro(cd_livro),
+foreign key(id_usuario) references tb_usuario(cd_usuario)
 )default character set = utf8;
 
-create table tb_endereco(
-cd_endereco int primary key auto_increment,
-cep_endereco VARCHAR(20) not null,
-ds_endereco VARCHAR(60) not null,
-complemento_endereco VARCHAR(45),
-id_usuario int,
-foreign key(id_usuario) references tb_usuario(cd_usuario)
-);
+create table tb_item_compra(
+id_livro int,
+id_compra int,
+vl_livro real not null,
+qt_livro VARCHAR(45) not null,
+foreign key(id_livro) references tb_livro(cd_livro),
+foreign key(id_compra) references tb_compra(cd_compra)
+)default character set = utf8;
+
+create table tb_foto(
+cd_foto int primary key auto_increment,
+nm_foto VARCHAR(120) not null,
+id_livro int,
+foreign key (id_livro) references tb_livro(cd_livro)
+)default character set = utf8;
+
+
+
+
+
+
+
+
+insert into tb_autor values (null, "Tolkien","tolkien.png");
+insert into tb_autor values (null, "Stephiane Meyer","stephane.png");
+insert into tb_autor values (null, "C.S Lewis","lewis.png");
+
+insert into tb_categoria values (null, "Ação","Tiroteios");
+insert into tb_categoria values (null, "Aventura","Guerras e animais em Nárnia");
+
+
+insert into tb_editora values (null, "Martins Fontes","martins.png");
+insert into tb_editora values (null, "Harler Coppins","Harler.png");
+
+
+insert into tb_livro values (null, "Narnia", "Portugues",1111222,"2000",40,35,1,1,700,100,80,1,3);
+
+
+select * from tb_autor;
+select * from tb_editora;
+select * from tb_livro;
+
